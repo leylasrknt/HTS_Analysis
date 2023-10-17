03280a934a9e59a3a86a75d75792ad989524aff0
Leyla Sirkinti <77142451+leylasrknt@users.noreply.github.com>
2023-10-17 13:58:50 +0200

    Update fastqdump.sh

--- fastqdump.sh
+++ fastqdump.sh
@@ -10,13 +10,4 @@ $fastqDir="/path/to/your/output_directory" # Replace with the desired output dir
 # Run fasterq-dump to download and split SRA file
 $fastqDump --split-3 --outdir $fastqDir $sraFile
 
-##loop for multiple SRA file
-# Output directory
-outputDir="/path/to/your/output_directory" # Replace with your desired output directory
 
-# Read SRA accession numbers from the file and download FASTQ files
-while IFS= read -r accession; do
-    $fastqDump --split-3 --outdir "$outputDir" "$accession"
-done < sra_accessions.txt
-
-echo "Download of all SRA files completed."
