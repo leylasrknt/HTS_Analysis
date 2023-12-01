29c87a9f3f0b2e8044241d1eccc034a94b3bdba4
Leyla Sirkinti <77142451+leylasrknt@users.noreply.github.com>
2023-12-01 10:26:31 +0100

    Update fastqc_trimmed.sh

--- fastqc_trimmed.sh
+++ fastqc_trimmed.sh
@@ -3,19 +3,14 @@
 # Activate the conda environment named "fastqc"
 conda activate fastqc
 
-# List of trimmed fastq files (replace these with your actual filenames)
-fastq_files=(
-    "sample1_trimmed.fastq"
-    "sample2_trimmed.fastq"
-    "sample3_trimmed.fastq"
-    "sample4_trimmed.fastq"
-)
+# Pattern for fastq files
+fastq_pattern="/path/to/trimmed_files/*.fastq"
 
-# Output directory for FastQC results (replace this with your desired output path)
+# Output directory for FastQC results 
 output_directory="/path/to/output_directory"
 
-# Run FastQC for each trimmed fastq file
-for file in "${fastq_files[@]}"; do
+# Run FastQC for each  fastq file matching the pattern
+for file in $fastq_pattern; do
     # Run FastQC on the current file and save the results to the specified output directory
     fastqc "$file" -o "$output_directory"
 done
