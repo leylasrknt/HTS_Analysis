612252d2a1daa0f7f15b91ca32822f731f95d617
Leyla Sirkinti <77142451+leylasrknt@users.noreply.github.com>
2023-12-03 18:10:20 +0100

    Update Picard.sh

--- Picard.sh
+++ Picard.sh
@@ -6,34 +6,29 @@ bam_dir="/path/to/your/bam/directory"
 # Directory to save the Picard results
 output_dir="/path/to/your/output/directory/Picard_Results_Optical_Duplication"
 
-# List of your BAM files (without the full path)
-bam_files=(
-    "file1.sorted.bam"
-    "file2.sorted.bam"
-    "file3.sorted.bam"
-    # Add more BAM files as needed
-)
-
 # Create the output directory if it doesn't exist
 mkdir -p "$output_dir"
 
-# Loop through each BAM file and run Picard's MarkDuplicates command
-for bam_file in "${bam_files[@]}"; do
-    # Define the full path to the BAM file
-    full_bam_path="$bam_dir/$bam_file"
-    # Define the output file name (with the output directory)
-    output_bam="$output_dir/$(basename "${full_bam_path%.sorted.bam}.marked.bam")"
-    # Define the metrics file name (with the output directory)
-    metrics_file="$output_dir/$(basename "${full_bam_path%.sorted.bam}.metrics.txt")"
+# Loop through each BAM file in the specified directory
+for bam_file in "$bam_dir"/*.sorted.bam; do
+    # Check if there are any matching files
+    if [ -e "$bam_file" ]; then
+        # Define the output file name (with the output directory)
+        output_bam="$output_dir/$(basename "${bam_file%.sorted.bam}.marked.bam")"
+        # Define the metrics file name (with the output directory)
+        metrics_file="$output_dir/$(basename "${bam_file%.sorted.bam}.metrics.txt")"
 
-    echo "Processing $full_bam_path..."
+        echo "Processing $bam_file..."
 
-    # Run Picard's MarkDuplicates command
-    picard MarkDuplicates \
-        INPUT="$full_bam_path" \
-        OUTPUT="$output_bam" \
-        METRICS_FILE="$metrics_file" \
-        REMOVE_DUPLICATES=true
+        # Run Picard's MarkDuplicates command
+        picard MarkDuplicates \
+            INPUT="$bam_file" \
+            OUTPUT="$output_bam" \
+            METRICS_FILE="$metrics_file" \
+            REMOVE_DUPLICATES=true
 
-    echo "Done processing $full_bam_path"
+        echo "Done processing $bam_file"
+    else
+        echo "No BAM files found in $bam_dir"
+    fi
 done
