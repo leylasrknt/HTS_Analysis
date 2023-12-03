b462466cc9e84376b9609fd59fcfc05d6e02d31c
Leyla Sirkinti <77142451+leylasrknt@users.noreply.github.com>
2023-12-03 23:17:57 +0100

    Update hisat2_to_run.sh

--- hisat2_to_run.sh
+++ hisat2_to_run.sh
@@ -8,34 +8,33 @@ FASTQ_DIR="path/to/your/trimmed/fastq/files"
 # Create the output directory if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
 
+# List of sample numbers
+SAMPLES=("abbreviations")
+
 # Get the list of trimmed FASTQ files in the specified directory
-TRIMMED_FILES=("$FASTQ_DIR"/*_1.fastq.gz)
+# Loop through each sample and perform alignment
+for i in "${SAMPLES[@]}"; do
+    echo "Processing SRR99375$i..."
 
-# Loop through each pair of trimmed FASTQ files and perform alignment
-for trimmed_file_1 in "${TRIMMED_FILES[@]}"; do
-    # Assuming the files are named consistently with "_1" and "_2" for the first and second reads
-    trimmed_file_2="${trimmed_file_1/_1/_2}"
-
-    echo "Processing $trimmed_file_1 and $trimmed_file_2..."
-
-    # Extract the sample identifier from the file name (modify as needed)
-    sample_id=$(basename "$trimmed_file_1" | sed 's/sample_\([0-9]*\)_1.*\.fastq/\1/')
+    # Define the file names for the current sample
+    TRIMMED_R1="${FASTQ_DIR}/SRR99375${i}_1_trimmed.fastq"
+    TRIMMED_R2="${FASTQ_DIR}/SRR99375${i}_2_trimmed.fastq"
 
     # Run HISAT2 for paired-end data
-    hisat2 -x "$INDEX_DIR/genome_index" -1 "$trimmed_file_1" -2 "$trimmed_file_2" -S "$OUTPUT_DIR/sample_${sample_id}.sam"
+    hisat2 -x "$INDEX_DIR/genome_index" -1 "$TRIMMED_R1" -2 "$TRIMMED_R2" -S "$OUTPUT_DIR/SRR99375${i}.sam"
 
     # Convert SAM to BAM
-    samtools view -b -o "$OUTPUT_DIR/sample_${sample_id}.bam" "$OUTPUT_DIR/sample_${sample_id}.sam"
+    samtools view -b -o "$OUTPUT_DIR/SRR99375${i}.bam" "$OUTPUT_DIR/SRR99375${i}.sam"
 
     # Sort and index the BAM file
-    samtools sort -o "$OUTPUT_DIR/sample_${sample_id}.sorted.bam" "$OUTPUT_DIR/sample_${sample_id}.bam"
-    samtools index "$OUTPUT_DIR/sample_${sample_id}.sorted.bam"
+    samtools sort -o "$OUTPUT_DIR/SRR99375${i}.sorted.bam" "$OUTPUT_DIR/SRR99375${i}.bam"
+    samtools index "$OUTPUT_DIR/SRR99375${i}.sorted.bam"
 
     # Remove intermediate files (SAM and unsorted BAM)
-    rm "$OUTPUT_DIR/sample_${sample_id}.sam"
-    rm "$OUTPUT_DIR/sample_${sample_id}.bam"
+    rm "$OUTPUT_DIR/SRR99375${i}.sam"
+    rm "$OUTPUT_DIR/SRR99375${i}.bam"
 
-    echo "Sample $sample_id completed."
+    echo "SRR99375$i completed."
 done
 
 echo "All alignment jobs completed."
